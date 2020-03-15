a297db3423e0bee840f728db60c4c8ebbc6425eb
gennadiy123 <shelukhin_g@ukr.net>
2020-03-15 21:16:31 +0200

    Updates

---
+++ precache-manifest.7bd231478943ba03017a5253192fd25f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf43d7c52354061882e6e7d1d4bbbb6",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f62bd2dc1f3126889d27",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e3bc92f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e3bc92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf87d2c47bcb568d37f",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3b636455.chunk.js"
+  },
+  {
+    "revision": "8f295445564cac0e313f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.23cf4f98.js"
+  }
+]);
